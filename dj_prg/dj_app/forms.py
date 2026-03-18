775d77428c98afa0d6069bb2f3b3056b5b406856
MaksRus777 <mizyuk.maks@mail.ru>
2026-03-18 22:47:23 +0300

    model2.0

--- dj_prg/dj_app/forms.py
+++ dj_prg/dj_app/forms.py
@@ -26,6 +26,9 @@ from django import forms
 # forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 
+# Релеционные БД (Старее, Все храниться в таблицах) - PostgresSQL, MySQL, SQLite
+# Нереляционные БД (Новее, JSON) - MongoDB
+
 
 # Создание формы №1 (Основные поля)
 class UserForm(forms.Form):
@@ -46,15 +49,15 @@ class UserForm(forms.Form):
     file_path = forms.FilePathField(label="Путь к файлу", required=False, match=".py$", recursive=True, allow_folders=True, path=r"C:\Users\Teacher\Desktop\TeacherPython", widget=forms.Select(attrs={'class': 'form-control'}))
 
 
-# Создание формы №1 (Форма авторизации)
+# Создание формы №2 (Форма авторизации)
 class AuthorizationForm(forms.Form):
     email = forms.EmailField(label="Email", widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'name@example.com'}))
     password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
 
 
-class  OperationForm(forms.Form):
-    pk = forms.IntegerField(label="ID", required=False,widget=forms.NumberInput(attrs={'class': 'form-control'}))
-    name = forms.CharField(label = "Имя", widget=forms.TextInput(attrs={'class': 'form-control'}))
-    age = forms.IntegerField(label="Возраст", help_text="Введите возраст", required=False, widget=forms.NumberInput(attrs={'class': 'form-control'}))
-
+# Создание формы №3 (Форма авторизации)
+class OperationsForm(forms.Form):
+    pk = forms.IntegerField(label="ID", required=False, widget=forms.NumberInput(attrs={'class': 'form-control'}))
+    name = forms.CharField(label="Имя", required=True, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    age = forms.IntegerField(label="Возраст", required=True, widget=forms.NumberInput(attrs={'class': 'form-control'}))
 
--- dj_prg/dj_app/views.py
+++ dj_prg/dj_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from dj_app.forms import *
-from dj_app.models import *
+from dj_app.forms import *  # Импорт всех ФОРМ
+from dj_app.models import *  # Импорт всех МОДЕЛЕЙ
 
 
 # Главная страница
@@ -39,21 +39,45 @@ def index(request):
     return None  # Заглушка чтобы PEP 8 не ругался
 
 
+# Метод для работы с операциями БД
 def operation(request):
+    # ЛОГИКА 1. [Клиент Обновил Страницу] - Когда клиент хочет посмотреть
     if request.method == "GET":
-        form = OperationForm()
 
+        # 1. Создание формы
+        form = OperationsForm()
 
+        # 2. Прочитать из БД всех людей
         person = Person.objects.all()
-        context ={'FORM': form, "TABLE": person}
-        return render(request, "operation.html", context = context)
+
+        # 3. Показать страницу
+        context = {"FORM": form, "TABLE": person}
+        return render(request, "operation.html", context=context)
+
+    # ЛОГИКА 2. [Клиент Нажал Кнопку Войти] - Когда клиент чтото хочет отправить
     if request.method == "POST":
-        pk = request.POST.get('pk')
-        name = request.POST.get('name')
-        age = request.POST.get('age')
-        form = OperationForm()
-        Person.objects.create(name=name, age=age)
 
+        # 1. Вытянули данные
+        pk = request.POST.get("pk")
+        name = request.POST.get("name")
+        age = request.POST.get("age")
+
+        # 2. Записать данные в БД
+        Person.objects.create(name=name, age=age)  # !!!
+
+        # 3. Создание формы
+        form = OperationsForm()
+
+        # 4. Прочитать из БД всех людей
         person = Person.objects.all()
-        context ={'FORM': form, "TABLE": person}
-        return render(request, "operation.html", context = context)
+
+        # 5. Показать страницу
+        context = {"FORM": form, "TABLE": person}
+        return render(request, "operation.html", context=context)
+
+
+        # !!! Исправить create подсветку
+
+
+
+    return None  # Заглушка чтобы PEP 8 не ругался
