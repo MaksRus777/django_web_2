6cee804cfb37591278720383a9de971c12a6d8c8
MaksRus777 <mizyuk.maks@mail.ru>
2026-04-21 22:57:08 +0300

    lesson10

--- dj_prg/dj_app/views.py
+++ dj_prg/dj_app/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from dj_app.forms import *  # Импорт всех ФОРМ
-from dj_app.models import *  # Импорт всех МОДЕЛЕЙ
+from dj_app.forms import *
 
 
 # Главная страница
@@ -44,40 +43,14 @@ def operation(request):
     # ЛОГИКА 1. [Клиент Обновил Страницу] - Когда клиент хочет посмотреть
     if request.method == "GET":
 
-        # 1. Создание формы
+        # Создание формы
         form = OperationsForm()
 
-        # 2. Прочитать из БД всех людей
-        person = Person.objects.all()
-
-        # 3. Показать страницу
-        context = {"FORM": form, "TABLE": person}
+        context = {"FORM": form}
         return render(request, "operation.html", context=context)
 
     # ЛОГИКА 2. [Клиент Нажал Кнопку Войти] - Когда клиент чтото хочет отправить
     if request.method == "POST":
-
-        # 1. Вытянули данные
-        pk = request.POST.get("pk")
-        name = request.POST.get("name")
-        age = request.POST.get("age")
-
-        # 2. Записать данные в БД
-        Person.objects.create(name=name, age=age)  # !!!
-
-        # 3. Создание формы
-        form = OperationsForm()
-
-        # 4. Прочитать из БД всех людей
-        person = Person.objects.all()
-
-        # 5. Показать страницу
-        context = {"FORM": form, "TABLE": person}
-        return render(request, "operation.html", context=context)
-
-
-        # !!! Исправить create подсветку
-
-
+        pass
 
     return None  # Заглушка чтобы PEP 8 не ругался
